828ff82592c6685134956b43486425d3029ae5d2
Robetzen <zarlos11111@gmail.com>
2023-03-09 23:37:14 -0400

    html and css

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_wolrd():
-  return "Hello world"
+  return render_template('home.html')
 
 
 print(__name__)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
