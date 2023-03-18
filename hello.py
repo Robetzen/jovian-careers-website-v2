5997c49ec557ef2dd258b0f9ec77f44567d064b0
Robetzen <zarlos11111@gmail.com>
2023-03-18 01:04:06 -0400

    cambia al html

--- hello.py
+++ hello.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
 
+JOBS = [
+   {
+     'id': 1,
+     'title': 'Data Analyst',
+     'location': 'Bengaluru, Idia',
+     'salary': 'Rs. 10,00,000'
+   },
+   
+     {
+     'id': 2,
+     'title': 'Data Scientist',
+     'location': 'Bengaluru, Idia',
+     'salary': 'Rs. 15,00,000'
+   },
+     
+      {
+     'id': 3,
+     'title': 'Frontend Engineer',
+     'location': 'Bengaluru, Idia',
+     'salary': 'Rs. 12,00,000'
+   },
+      
+      {
+     'id': 4,
+     'title': 'Backend Engineer',
+     'location': 'San Francisco',
+     'salary': '120,000'
+   },  
+   
+  
+]
+
 @app.route('/')
 def hello_wolrd():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 
 print(__name__)
