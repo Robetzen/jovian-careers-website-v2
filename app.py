50ea08552940a7b39af5830f2ae633be5ad11c4a
Robetzen <zarlos11111@gmail.com>
2023-04-13 06:06:55 -0400

    muchos cambios

--- app.py
+++ app.py
@@ -1,51 +1,43 @@
-from flask import Flask, render_template, jsonify 
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_jovian():
+  jobs = load_jobs_from_db()
+  return render_template('home.html', 
+                         jobs=jobs)
 
-JOBS = [
-   {
-     'id': 1,
-     'title': 'Data Analyst',
-     'location': 'Bengaluru, Idia',
-     'salary': 'Rs. 10,00,000'
-   },
-   
-     {
-     'id': 2,
-     'title': 'Data Scientist',
-     'location': 'Bengaluru, Idia',
-     'salary': 'Rs. 15,00,000'
-   },
-     
-      {
-     'id': 3,
-     'title': 'Frontend Engineer',
-     'location': 'Bengaluru, Idia',
-     'salary': 'Rs. 12,00,000'
-   },
-      
-      {
-     'id': 4,
-     'title': 'Backend Engineer',
-     'location': 'San Francisco',
-     'salary': '120,000'
-   },  
-   
-  
-]
-
-@app.route('/')
-def hello_wolrd():
-  return render_template('home.html', jobs=JOBS)
-
-@app.route('/api/jobs')
+@app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
   
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', 
+                         job=job)
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 
-print(__name__)
-
-if __name__ == "__main__":
-  app.run(debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,1 +1,23 @@
 import sqlalchemy
+from sqlalchemy  import create_engine, text
+
+db_connection_string = "mysql+pymysql://ths2foh978slwyq2p9kg:pscale_pw_TWs5KenEcWYhcfIVs4On2v8jtBLBwazE5iN9Ub8S1EV@us-east.connect.psdb.cloud/webproject?charset=utf8mb4"
+
+engine = create_engine(db_connection_string, connect_args={
+    "ssl":{
+         "ssl_ca": "/etc/ssl/cert.pem"
+    }
+})
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row))
+    return jobs
+
+with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    print(result.all())
